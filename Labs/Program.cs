447c8a138cb4e209f19906b7244728328fe87398
kirril <kirk.andri@gmail.com>
2020-02-07 14:44:13 +0200

    Lab_1 confirm and done
    ###
     __
    /   \
    b_d
    \__/

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Labs
 {
     class Program
     {
+        private static volatile bool stepHello = true, stepWorld = false;
         static void Main(string[] args)
         {
+            // создаем новый поток
+            Thread myThreadHello = Thread.CurrentThread;
+            Thread myThreadWorld = new Thread(new ThreadStart(World));
+
+            myThreadHello.Name = "Hello";
+            myThreadWorld.Name = "World";
+
+            myThreadWorld.Start();
+
+            Hello();
+            Console.ReadLine();
+        }
+        public static void Hello() {
+            while (true) {
+                try {
+                    if (stepHello) {
+                        stepHello = false;
+                        Console.Write("Hello ");
+                        stepWorld = true;
+                    }
+                } catch (Exception) { throw; }
+            }
+        }
+        public static void World() {
+            while (true) {
+                try {
+                    if (stepWorld) {
+                        stepWorld = false;
+                        Console.WriteLine("World !!");
+                        stepHello = true;
+                    }
+                } catch (Exception) { throw; }
+            }
         }
     }
 }
