8ab7884f561a610d67a8112965f080568d77edeb
kirril <kirk.andri@gmail.com>
2020-02-20 12:39:45 +0200

    add lab_2 tast

---
+++ Lab_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        class C
+        {
+            private int n=0, m=0;
+
+            public C(string x)
+            {
+                n = 0;
+            }
+        }
+
+        Dictionary<string, string> cache = new Dictionary<string, string>();
+        public string Cache(string x)
+        {
+            if (!cache.ContainsKey(x)) cache.Add(x, string.Format("{0}", new C(x)));
+            return cache[x];
+        }
+        public void Uncache(string x) { cache.Remove(x); }
+        public string Eval(string x) { return string.Format("{0}", new C(Cache(x))); }
+
+
+        static void Main()
+        {
+            var c = new Program();
+            for (var s = Console.ReadLine(); s != null; s = Console.ReadLine())
+            {
+                var a = s.Split(' ');
+                var y = "0.0000000000";
+                if (a[1] == "Cache") y = c.Cache(a[2]);
+                else if (a[1] == "Uncache") c.Uncache(a[2]);
+                else if (a[1] == "Eval") y = c.Eval(a[2]);
+                if (a[0] == "Verbose") Console.WriteLine(y); 
+            }
+        }
+
+    }
+}
